700f5ddabc284e4877c5304639b738f4d50ae9c8
rafaelFacundo <rafaelfacundo63@gmail.com>
2026-07-24 06:52:05 -0300

    DB creation changed to a single transaction

--- src/main/db/index.ts
+++ src/main/db/index.ts
@@ -4,55 +4,62 @@ import { applicationDatabasePath } from "../utils/paths";
 let dataBase: sqlite3.DatabaseSync;
 
 export const openDatabase = (): void => {
+  dataBase = new sqlite3.DatabaseSync(applicationDatabasePath);
   try {
-    dataBase = new sqlite3.DatabaseSync(applicationDatabasePath);
-    dataBase.exec(`CREATE TABLE IF NOT EXISTS settings (
+    //console.log("ADAD", dataBase);
+    dataBase.exec(`
+      BEGIN TRANSACTION;
+
+      CREATE TABLE IF NOT EXISTS settings (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         key TEXT UNIQUE,
         value TEXT NOT NULL
       );
-    `);
-    dataBase.exec(`CREATE TABLE IF NOT EXISTS folders (
+
+      CREATE TABLE IF NOT EXISTS folders (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         path TEXT UNIQUE,
         last_scan TIMESTAMP NOT NULL
       );
-    `);
-    dataBase.exec(`CREATE TABLE IF NOT EXISTS movies (
+
+      CREATE TABLE IF NOT EXISTS movies (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
         path TEXT UNIQUE,
-        durantion INTEGER,
+        duration INTEGER,
         watched INTEGER,
         last_opened TIMESTAMP,
         number_of_cards INTEGER,
-        las_modified TIMESTAMP,
+        last_modified TIMESTAMP,
         size INTEGER,
         folder_id INTEGER,
         FOREIGN KEY(folder_id) REFERENCES folders(id)
       );
-    `);
-    dataBase.exec(`CREATE TABLE IF NOT EXISTS thumbs (
+      
+      CREATE TABLE IF NOT EXISTS thumbs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         path TEXT UNIQUE,
         movie_id INTEGER,
         FOREIGN KEY(movie_id) REFERENCES movies(id)
       );
-    `);
-    dataBase.exec(`CREATE TABLE IF NOT EXISTS subtitles (
+
+      CREATE TABLE IF NOT EXISTS subtitles (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         path TEXT UNIQUE
       );
-    `);
-    dataBase.exec(`CREATE TABLE IF NOT EXISTS movies_subtitles (
+
+      CREATE TABLE IF NOT EXISTS movies_subtitles (
         movie_id INTEGER,
         subtitle_id INTEGER,
         FOREIGN KEY(movie_id) REFERENCES movies(id),
         FOREIGN KEY(subtitle_id) REFERENCES subtitles(id)
       );
+
+      COMMIT;
     `);
   } catch (error) {
     console.log("AN ERROR OCURRED WHILE CREATING THE DATABASE");
+    dataBase.exec(`ROLLBACK`);
     console.log(error);
   }
 };
